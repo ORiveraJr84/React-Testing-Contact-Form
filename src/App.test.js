144cdf649d1043ad783b05edf0bd14bbb898284f
ORiveraJr84 <oriverajr84@gmail.com>
2020-11-09 00:12:27 -0500

    MVp attained.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
+import { act } from "react-dom/test-utils";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  render(<ContactForm />);
+});
+
+test("All form fields working", async () => {
+  render(<ContactForm />);
+
+  const firstName = screen.getByTestId(/firstName/i);
+  fireEvent.change(firstName, { target: { value: "Test" } });
+  expect(firstName).toHaveValue("Test");
+
+  const lastName = screen.getByTestId(/lastName/i);
+  fireEvent.change(lastName, { target: { value: "Test" } });
+  expect(lastName).toHaveValue("Test");
+
+  const email = screen.getByTestId(/email/i);
+  fireEvent.change(email, { target: { value: "Test" } });
+  expect(email).toHaveValue("Test");
+
+  const message = screen.getByTestId(/message/i);
+  fireEvent.change(message, { target: { value: "Test" } });
+  expect(message).toHaveValue("Test");
+
+  const button = screen.getByTestId(/button/i);
+  act(() => {
+    button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+  });
 });
